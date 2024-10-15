2efe0f8468dfcf65511d2623618e24d16a9b74c8
Audrey Soulet <asoulet@insa-lyon.fr>
2024-10-15 10:34:44 +0200

    setup SpringBoot

---
+++ src/main/java/Hexa/PLD_Agile/PldAgileApplication.java
@@ -0,0 +1,13 @@
+package Hexa.PLD_Agile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PldAgileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PldAgileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hexa/PLD_Agile/PldAgileApplicationTests.java
@@ -0,0 +1,13 @@
+package Hexa.PLD_Agile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PldAgileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
