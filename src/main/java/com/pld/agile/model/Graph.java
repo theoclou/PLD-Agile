caca5ec3f494795381164cee6a425cb8e666ce35
Quentin Mariatgit <quentin.mariat@insa-lyon.fr>
2024-10-17 16:26:41 +0200

    modification de graph, et ajout d'un main pour le tester (marche pas)

--- src/main/java/com/pld/agile/model/Graph.java
+++ src/main/java/com/pld/agile/model/Graph.java
@@ -9,43 +9,72 @@ import javax.swing.text.Segment;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 class Graph {
-    private Map<Intersection, List<Section>> adjacencyList = new Map<>();
+    private Map<Intersection, List<Section>> adjencyList = new HashMap<>();
+    private Map<String, Intersection> intersectionMap = new HashMap<>();  // Pour rechercher les intersections par ID
 
-    public void loadFromXml(String filePath) {
+    public void readXml(String filePath) {
         try {
             File xmlFile = new File(filePath);
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document document = builder.parse(xmlFile);
 
-            NodeList nodeElements = document.getElementsByTagName("noeud");
-            for (int i = 0; i < nodeElements.getLength(); i++) {
-                Element element = (Element) nodeElements.item(i);
+            // Lecture des intersections
+            NodeList intersectionElements = document.getElementsByTagName("noeud");
+            for (int i = 0; i < intersectionElements.getLength(); i++) {
+                Element element = (Element) intersectionElements.item(i);
                 String id = element.getAttribute("id");
                 double latitude = Double.parseDouble(element.getAttribute("latitude"));
                 double longitude = Double.parseDouble(element.getAttribute("longitude"));
-                nodes.add(new Node(id, latitude, longitude));
+                
+                // Création de l'objet Intersection
+                Intersection intersection = new Intersection(id, latitude, longitude);
+                adjencyList.put(intersection, new ArrayList<>());  // Initialiser une liste vide pour chaque intersection
+                intersectionMap.put(id, intersection);  // Sauvegarde dans la Map pour référence facile
             }
 
-            NodeList segmentElements = document.getElementsByTagName("troncon");
-            for (int i = 0; i < segmentElements.getLength(); i++) {
-                Element element = (Element) segmentElements.item(i);
-                String origin = element.getAttribute("origine");
-                String destination = element.getAttribute("destination");
+            // Lecture des tronçons
+            NodeList sectionElements = document.getElementsByTagName("troncon");
+            for (int i = 0; i < sectionElements.getLength(); i++) {
+                Element element = (Element) sectionElements.item(i);
+                String originId = element.getAttribute("origine");
+                String destinationId = element.getAttribute("destination");
                 double length = Double.parseDouble(element.getAttribute("longueur"));
-                String streetName = element.getAttribute("nomRue");
-                segments.add(new Segment(origin, destination, length, streetName));
+                String name = element.getAttribute("nomRue");
+                
+                // Recherche des intersections d'origine et de destination
+                Intersection origin = intersectionMap.get(originId);
+                Intersection destination = intersectionMap.get(destinationId);
+
+                // Vérification que les intersections existent
+                if (origin != null && destination != null) {
+                    // Création du tronçon et ajout à la liste d'adjacence
+                    Section section = new Section(origin, destination, name, length);
+                    adjencyList.get(origin).add(section);
+                } else {
+                    System.err.println("Erreur : Intersection manquante pour origine " + originId + " ou destination " + destinationId);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    // Getters for nodes and segments
-    public List<Node> getNodes() { return nodes; }
-    public List<Segment> getSegments() { return segments; }
+    // Méthode pour afficher le graphe
+    public void displayGraph() {
+        for (Map.Entry<Intersection, List<Section>> entry : adjencyList.entrySet()) {
+            Intersection intersection = entry.getKey();
+            List<Section> sections = entry.getValue();
+            System.out.println("Intersection " + intersection + " a les tronçons suivants : ");
+            for (Section section : sections) {
+                System.out.println("    " + section);
+            }
+        }
+    }
 }
