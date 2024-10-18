666e5cc23645b6e7a87971e31502cbbb593e6bb7
Théo Clouscard <theo.clouscard@insa-lyon.fr>
2024-10-18 11:12:35 +0200

    added Round Class to manage all

---
+++ src/main/java/com/pld/agile/model/Round.java
@@ -0,0 +1,33 @@
+package com.pld.agile.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class Round {
+    private List<Courier> courierList;
+    private List<DeliveryRequest> deliveryRequestList;
+    private Map<Courier, DeliveryTour> tourAttribution;
+
+    public Round() {}
+
+    public void init(List<Courier> courierList, List<DeliveryRequest> deliveryRequestList) {
+        this.courierList = courierList;
+        this.deliveryRequestList = deliveryRequestList;
+    }
+
+    public Map<Courier, DeliveryTour> getTourAttribution() {
+        return tourAttribution;
+    }
+
+    public void computeRound(){
+        // TODO
+        // Tant que la liste de Delivery Request n'est pas vide, on va
+        // lancer un calcul de graph, créer un DeliveryTour avec le résultat, l'affecter
+        // à un Courier et mettre à jour TourAttribution, puis supprimer les DeliveryRequest qu'on a utilisé
+        // de la liste
+    }
+
+
+
+
+}
