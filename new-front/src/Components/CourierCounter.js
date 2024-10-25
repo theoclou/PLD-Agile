730843abb54ee5ea167a1c70f636ed3ecb0bde6c
Théo Clouscard <theo.clouscard@insa-lyon.fr>
2024-10-25 10:30:40 +0200

    deleted a r in courrier

--- new-front/src/Components/CourierCounter.js
+++ new-front/src/Components/CourierCounter.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import './CourierCounter.css';
+import React from "react";
+import "./CourierCounter.css";
 
 const CourierCounter = ({ count, setCount }) => {
-    const increment = () => {
-        if (count < 10) {
-            setCount(prevCount => prevCount + 1);
-        }
-    };
+  const increment = () => {
+    if (count < 10) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  };
 
-    const decrement = () => {
-        if (count > 2) {
-            setCount(prevCount => prevCount - 1);
-        }
-    };
+  const decrement = () => {
+    if (count > 2) {
+      setCount((prevCount) => prevCount - 1);
+    }
+  };
 
-    return (
-        <div className="courier-counter">
-            <h2>Courriers: {count}</h2>
-            <button onClick={decrement}>-</button>
-            <button onClick={increment}>+</button>
-        </div>
-    );
+  return (
+    <div className="courier-counter">
+      <h2>Couriers: {count}</h2>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
+    </div>
+  );
 };
 
 export default CourierCounter;
