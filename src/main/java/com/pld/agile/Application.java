430cbd4f3f4b406b3cb4b14334f1e60723dfcd61
a1bderrahmane <abderrahmaneetudier@gmail.com>
2024-11-06 09:08:45 +0100

    testing the clustering

--- src/main/java/com/pld/agile/Application.java
+++ src/main/java/com/pld/agile/Application.java
@@ -1,7 +1,14 @@
 package com.pld.agile;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.pld.agile.model.entity.Courier;
+import com.pld.agile.model.entity.Round;
+import com.pld.agile.model.graph.Plan;
 
 @SpringBootApplication
 public class Application {
@@ -10,12 +17,11 @@ public class Application {
 	public static void main(String[] args) {
 
 		// Launch App
-		SpringApplication.run(Application.class, args);
+		// SpringApplication.run(Application.class, args);
 
-		/*
 		Plan plan = new Plan();
 
-		String filePath = "src/data/petitPlan.xml";
+		String filePath = "src/data/grandPlan.xml";
 		try {
 			plan.readXml(filePath);
 		} catch (Exception e) {
@@ -23,32 +29,34 @@ public class Application {
 			System.exit(1); // Arrêter le programme avec un code d'erreur
 		}
 
-		plan.preprocessData();
-		List<Integer> vertices = Arrays.asList(0, 256, 233, 127);
+		// plan.preprocessData();
+		// List<Integer> vertices = Arrays.asList(0, 256, 233, 127);
 		/*
 		 * If using a list of ids use instead this :
 		 * List<Integer> vertices =plan.formatInput(List<String> idIntersections)
 		 */
-		/*
-		Solver solver = new Solver(plan, vertices, new BnBStrategy());
-		solver.init();
-		solver.solve();
-		List<Integer> bestPath = solver.getBestPath();
-		System.out.println(bestPath);
-		plan.computeTour(bestPath);
-		System.out.println("finished");
 
+		// Solver solver = new Solver(plan, vertices, new BnBStrategy());
+		// solver.init();
+		// solver.solve();
+		// List<Integer> bestPath = solver.getBestPath();
+		// System.out.println(bestPath);
+		// plan.computeTour(bestPath);
+		// System.out.println("finished");
 
 		// Création Round
 		Round round = new Round();
-		List<Courier> couriers = new ArrayList<>();
+		List<Courier> couriers;
+		couriers = new ArrayList<>();
 		round.init(2, plan);
-		String requestPath = "src/data/demandePetit2.xml";
+		String requestPath = "src/data/demandeGrand9.xml";
 		try {
-		round.loadRequests(requestPath);
+			round.loadRequests(requestPath);
 		} catch (Exception e) {
-		System.err.println("Erreur : " + e.getMessage());
+			System.err.println("Erreur : " + e.getMessage());
 		}
-		System.exit(0);*/
+		ArrayList<ArrayList<String>> groups = round.computeRoundOptimized();
+		System.out.println(groups);
+		System.exit(0);
 	}
 }
