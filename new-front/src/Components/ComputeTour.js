a5c83d50b48e488477499a4eb157712260c13595
Audrey <audrey.soulet@insa-lyon.fr>
2024-11-10 16:54:09 +0100

    compute button

--- new-front/src/Components/ComputeTour.js
+++ new-front/src/Components/ComputeTour.js
@@ -3,14 +3,14 @@ import React from "react";
 
 const ComputeTour = ({ onClick }) => {
   return (
-    <div className="buttonContainer">
+    <div className="button">
       <button
         id="compute-tour-button"
         className="inputField"
         style={{ display: "none" }}
         onClick={onClick}
       />
-      <label htmlFor="compute-tour-button" className="custom-file-upload">
+      <label htmlFor="compute-tour-button" className="button">
         Compute Tour
       </label>
     </div>
